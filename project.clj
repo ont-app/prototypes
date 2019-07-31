ac636dd64ac835e5ac42c550ef12ae4879c3c557
Eric <eric.scott@acm.org>
2019-07-31 16:58:17 -0700

    Renaming project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prototypes "0.1.0-SNAPSHOT"
+(defproject ont-app/prototypes "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
